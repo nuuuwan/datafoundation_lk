d4687cbb8de6dd7b90203e1927a0ae286f954b73
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-07 18:31:33 +0530

    Added  <Router> stub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Menu from './nonstate/Menu.js';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lanka Data Foundation</h1>
+      <Menu />
+
+      <Router>
+        <Switch>
+          <Route path="/home" >
+          </Route>
+        </Switch>
+      </Router>
+
+      <div className="div-copyright">
+        Copyright Lanka Data Foundation 2021
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
