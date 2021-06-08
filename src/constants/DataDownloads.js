33057c705f1c3d2bca87bb5cc59bab6835539a72
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-08 13:26:07 +0530

    commit

---
+++ src/constants/DataDownloads.js
@@ -0,0 +1,12 @@
+export const DATA_DOWNLOADS = [
+  {
+    'name': 'Provinces',
+    'url': 'https://raw.githubusercontent.com/nuuuwan/gig-data/master/province.tsv',
+    'tags': ['Administrative Regions'],
+  },
+  {
+    'name': 'Districts',
+    'url': 'https://raw.githubusercontent.com/nuuuwan/gig-data/master/district.tsv',
+    'tags': ['Administrative Regions'],
+  }
+]
