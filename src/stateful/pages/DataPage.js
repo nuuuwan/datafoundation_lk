8a5226b877e2dbeb9d756390cea206b5edc1a71e
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-08 11:45:01 +0530

    DataPage.js framework

--- src/stateful/pages/DataPage.js
+++ src/stateful/pages/DataPage.js
@@ -1,3 +1,12 @@
 export default function DataPage(props) {
-  return null;
+  return (
+    <div>
+      <h2>Download Public Data</h2>
+      <ul>
+        <li>Administrative Regions of Sri Lanka</li>
+        <li>Electoral Districts and Polling Divisions of Sri Lanka</li>
+        <li>Census Data</li>
+      </ul>
+    </div>
+  )
 }
