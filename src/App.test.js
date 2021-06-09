d7bb656964a30ba4cb01359cd8374d6139b86466
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-09 14:17:41 +0530

    Removed tests

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders title', () => {
-  const titleElem = screen.getByText('Lanka Data Foundation');
-  expect(titleElem).toBeInTheDocument();
-});
