490c9351e974477ca0337ba9617739b1f15476e6
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-09 13:08:54 +0530

    Updated server and CSS

--- src/core/Server.js
+++ src/core/Server.js
@@ -1,28 +1,19 @@
-import WWW from '../base/WWW.js';
+import WWW from 'base/WWW.js';
 
 function gerServerHost() {
   const { REACT_APP_SERVER_HOST } = process.env;
   return REACT_APP_SERVER_HOST;
 }
 
-function getServerPort(serverType) {
-  const SERVER_TYPE_TO_PORT = {
-    gig: 81,
-    geo: 82,
-  }
-  return SERVER_TYPE_TO_PORT[serverType];
-}
-
 export default class Server {
   static getURL(serverType, cmd, paramsList) {
     const host = gerServerHost();
-    const port = getServerPort(serverType);
-    return `http://${host}:${port}`
-      + `/${cmd}/${paramsList.join('/')}`
+    return `${host}/${serverType}_server/${cmd}/${paramsList.join('/')}`
   }
 
   static async run(serverType, cmd, paramsList) {
     const url = Server.getURL(serverType, cmd, paramsList);
+    console.debug(url);
     const data = await WWW.getJSON(url);
     return data;
   }
