fc6677dc23825892b7b6c888e8293150cd3e478c
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-10 13:50:00 +0530

    Added restart

--- scripts/local_start_servers_update_nginx.sh
+++ scripts/local_start_servers_update_nginx.sh
@@ -1,23 +1,36 @@
 nginx -s stop
 
-cp nginx/nginx-final-local.conf /usr/local/etc/nginx/nginx.conf
+echo '----------------------------------------------------------------'
 
 docker pull nuuuwan/gig_server
 docker pull nuuuwan/geo_server
 docker pull nuuuwan/mylocal
 docker pull nuuuwan/datafoundation_lk
 
+echo '----------------------------------------------------------------'
+
 docker kill local_gig_server
 docker kill local_geo_server
 docker kill local_mylocal
 docker kill local_datafoundation_lk
 
-docker run --rm -p 4001:4001 -d --name local_gig_server nuuuwan/gig_server
-docker run --rm -p 4002:4002 -d --name local_geo_server nuuuwan/geo_server
-docker run --rm -p 5002:80 -d --name local_mylocal nuuuwan/mylocal
-docker run --rm -p 5001:80 -d --name local_datafoundation_lk nuuuwan/datafoundation_lk
+docker rm local_gig_server
+docker rm local_geo_server
+docker rm local_mylocal
+docker rm local_datafoundation_lk
+
+echo '----------------------------------------------------------------'
+
+docker run --restart=unless-stopped -p 4001:4001 -d --name local_gig_server nuuuwan/gig_server
+docker run --restart=unless-stopped -p 4002:4002 -d --name local_geo_server nuuuwan/geo_server
+docker run --restart=unless-stopped -p 5002:80 -d --name local_mylocal nuuuwan/mylocal
+docker run --restart=unless-stopped -p 5001:80 -d --name local_datafoundation_lk nuuuwan/datafoundation_lk
+
+echo '----------------------------------------------------------------'
 
 docker ps
 
+echo '----------------------------------------------------------------'
+cp nginx/nginx-final-local.conf /usr/local/etc/nginx/nginx.conf
 nginx
 tail -f /usr/local/var/log/nginx/error.log
