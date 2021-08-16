14fc260152ee2930ae2218ab689dcd39177f26cd
umayanga <umayangag@gmail.com>
2021-08-17 00:17:23 +0530

    load about page images locally

--- src/constants/Profile.js
+++ src/constants/Profile.js
@@ -1,7 +1,7 @@
 export const PROFILES = {
   sanjiva: {
     name: 'Dr. Sanjiva Weerawarana',
-    imgSrc: 'https://media-exp3.licdn.com/dms/image/C4D03AQExvPTMWbL8MQ/profile-displayphoto-shrink_200_200/0/1516260591433?e=1628726400&v=beta&t=Q9M9STuG9RYS4Nk39w168T6yOyoF8Xi9EJhbayqY9hk',
+    imgSrc: 'people/sanjiva.png',
     designation: 'Director & Member',
     linkedinProfileUrl: 'https://www.linkedin.com/in/sanjivaweerawarana/',
     githubProfileUrl: 'https://github.com/sanjiva',
@@ -9,14 +9,14 @@ export const PROFILES = {
   },
   nishan: {
     name: 'Dr. Nishan de Mel',
-    imgSrc: 'https://media-exp3.licdn.com/dms/image/C5103AQEeKSw9Aw9CDA/profile-displayphoto-shrink_200_200/0/1527962029976?e=1628726400&v=beta&t=KHMqReDlmjV_8jwGGSJwdlfvGaL09nOBMVqeo5C-2eY',
+    imgSrc: 'people/nishand.png',
     designation: 'Director',
     linkedinProfileUrl: 'https://lk.linkedin.com/in/nishan-de-mel-60759822',
     twitterProfileUrl: 'https://twitter.com/NCdeMel',
   },
   nuwan: {
     name: 'Nuwan I. Senaratna',
-    imgSrc: 'https://media-exp1.licdn.com/dms/image/C5603AQEZOS3qXdvWLQ/profile-displayphoto-shrink_400_400/0/1546830418390?e=1628726400&v=beta&t=UaYYO_pn54E5-iOwIF5xlO6kg68JKN4oJZdbnhOKm_U',
+    imgSrc: 'people/nuwans.png',
     designation: 'Director',
     linkedinProfileUrl: 'https://www.linkedin.com/in/nuwansenaratna/',
     githubProfileUrl: 'https://github.com/nuuuwan',
@@ -24,13 +24,13 @@ export const PROFILES = {
   },
   sherazad: {
     name: 'Sherazad Hamit',
-    imgSrc: 'https://media-exp1.licdn.com/dms/image/C5603AQGCiD_4gp8feg/profile-displayphoto-shrink_400_400/0/1588186553320?e=1628726400&v=beta&t=sjSvDC-CludiqQUP6yYJKiZLspP-DGUfe9E8Xs_8DTE',
+    imgSrc: 'people/sherazad.png',
     designation: 'Program Director',
     linkedinProfileUrl: 'https://www.linkedin.com/in/sherazadhamit/',
   },
   umayanga: {
     name: 'Umayanga Gunawardhana',
-    imgSrc: 'https://media-exp1.licdn.com/dms/image/C5103AQHakla6e4dw0g/profile-displayphoto-shrink_400_400/0/1564405517365?e=1628726400&v=beta&t=Y8dnkkV9GRFJGWeKRAnWqrXuFsW2uB3MpbdhSk5uQvE',
+    imgSrc: 'people/umayangag.png',
     designation: 'Software Engineer',
     linkedinProfileUrl: 'https://www.linkedin.com/in/umayangag/',
     githubProfileUrl: 'https://github.com/umayangag',
@@ -38,7 +38,7 @@ export const PROFILES = {
 
   louiqa: {
     name: 'Dr. Louiqa Raschid',
-    imgSrc: 'https://www.cs.umd.edu/sites/default/files/images/article/2020/LRaschid_IMG.JPG',
+    imgSrc: 'people/louiqa.jpeg',
     designation: 'Member',
     linkedinProfileUrl: 'https://www.linkedin.com/in/louiqa-raschid-b76965',
     wikipediaProfileUrl: 'https://en.wikipedia.org/wiki/Louiqa_Raschid',
@@ -47,7 +47,7 @@ export const PROFILES = {
 
   verite: {
     name: 'Verite Research',
-    imgSrc: 'https://media-exp1.licdn.com/dms/image/C510BAQGHnWQJTVSpSw/company-logo_200_200/0/1527484559551?e=2159024400&v=beta&t=_3LRmK4Pd2_GySh9e9ck3QOwiYbcNZUPaLWANfJS9BU',
+    imgSrc: 'people/verite.png',
     designation: 'Member',
     websiteUrl: 'https://www.veriteresearch.org/',
     linkedinProfileUrl: 'https://www.linkedin.com/company/verit%C3%A9-research',
@@ -56,7 +56,7 @@ export const PROFILES = {
 
   shea: {
     name: 'Shea Wickramasingha',
-    imgSrc: 'https://media-exp3.licdn.com/dms/image/C5103AQFam17nqUt_Wg/profile-displayphoto-shrink_200_200/0/1525537346713?e=1628726400&v=beta&t=tThRfJaSsvDnxxIGdf5eBIHCRVCr4_JRFcGVH45146g',
+    imgSrc: 'people/shea.jpeg',
     designation: 'Member',
     linkedinProfileUrl: 'https://lk.linkedin.com/in/shea-wickramasingha-221a9b25',
   },
